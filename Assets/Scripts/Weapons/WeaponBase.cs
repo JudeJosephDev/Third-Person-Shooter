fe5a1eb2efa732ef17ff88b505a8eeba9eb22627
JudeJosephGD <JudeJoseph9@outlook.com>
2022-05-24 21:01:26 -0400

    Updated Task List & Comments
    Updated The Weapon Base to include Task list tasks (TODO).
    Updated the comments for each function in the Weapon Base

--- Assets/Scripts/Weapons/WeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -1,43 +1,61 @@
+//TODO: Create a series of Unity Events that will notify other systems (I.e. UI) of the state of the Weapon Component (I.e. OnWeaaponFired)
+//TODO: Create an enum for the firing mode of the weapon (Auto, Burst, Single, etc) 
+//TODO: Implement Firing timer to prevent player from firing faster than the fire rate of the weapon
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class WeaponBase : MonoBehaviour
 {
-    public WeaponData m_WeaponData;
-    [HideInInspector] public int m_ReserveAmmo;
-    public int m_AmmoInClip;
 
+    [SerializeField] private WeaponData m_WeaponData;
+    [SerializeField] private LayerMask m_HitLayerMask;
+
+    //Ammo
+    [HideInInspector] public int m_ReserveAmmo;
+    [HideInInspector] public int m_AmmoInClip;
+
+    [Header("Additional Weapon Data")]
     public AudioClip m_FiringSound;
     public AudioClip m_ReloadSound;
     public AudioClip m_OutOfAmmoSound;
     public GameObject m_Mesh;
     public Transform m_FiringPoint;
 
-    protected bool m_CanFire;
-    protected float m_CurrentMaxSpread;
+    protected bool m_CanFire = true; 
 
+    //Weapon Spread Data
+    protected float m_CurrentMaxSpread;
     protected Coroutine m_SpreadResetTimer;
     private static float SpreadResetDelay = 2.0f;
     private static float SpreadResetRate = 0.1f;
 
-    [SerializeField] private LayerMask m_HitLayerMask;
-    protected LayerMask GetHitLayer() { return m_HitLayerMask; }
+
+    public WeaponData WeaponData { get => m_WeaponData; set => m_WeaponData =  value ; }
+    protected LayerMask HitLayer { get => m_HitLayerMask; }
 
     protected void Awake() {
         if (!m_WeaponData) {
-            Application.Quit();
             throw new System.NullReferenceException($"{gameObject.name}'s Weapon component does not contain Weapon Data");
         }
     }
 
+    // Built In function that is called at the start of an object's lifetime.
+    /// <summary>
+    ///  Used to Initialize realtime Data from the Weapon Data Scriptable Object.
+    /// </summary>
     protected void Start() {
         m_AmmoInClip = m_WeaponData.m_AmmoPerClip;
         m_ReserveAmmo = m_WeaponData.m_AmmoPerClip * m_WeaponData.m_InitialClips;
         m_CurrentMaxSpread = m_WeaponData.m_BaseWeaponSpread;
     }
 
+    /// <summary>
+    /// Primary Fire for a weapon
+    /// </summary>
     public virtual void FireWeapon() {
+        //Ensure the player has enough ammo to use the weapon before firing.
         if (m_AmmoInClip > 0) {
 
             if (m_SpreadResetTimer != null) { StopCoroutine(m_SpreadResetTimer); }
@@ -48,7 +66,7 @@ public abstract class WeaponBase : MonoBehaviour
             FiringRay.direction = Direction;
             RaycastHit HitInfo;
 
-            if (Physics.Raycast(FiringRay, out HitInfo, m_WeaponData.m_Range, GetHitLayer(), QueryTriggerInteraction.Ignore)) {
+            if (Physics.Raycast(FiringRay, out HitInfo, m_WeaponData.m_Range, HitLayer, QueryTriggerInteraction.Ignore)) {
 
                 Ray RayFromMuzzle = new Ray(m_FiringPoint.position, HitInfo.point - m_FiringPoint.position);
                 RaycastHit RFMHitInfo;
@@ -67,8 +85,16 @@ public abstract class WeaponBase : MonoBehaviour
             Debug.DrawLine(FiringRay.origin, FiringRay.origin + FiringRay.direction * HitInfo.distance, Color.magenta, 10.0f, true);
             m_SpreadResetTimer = StartCoroutine(ResetSpread());
         }
+
+        //TODO: If there is not enough ammo in the clip, Play the m_OutOfAmmoSound Sound clip 
+        //TODO: If there is not enough ammo in the clip, Cancel the AutoFire Coroutine for AutoFire Weapons
+        //TODO: If there is not enough ammo in the clip, Call the Out of Ammo Event
     }
 
+    /// <summary>
+    /// Calculates a psuedo-random amount of spread at the weapons max range
+    /// </summary>
+    /// <returns></returns>
     protected Vector3 CalculateSpread() {
 
         Vector3 CurrentSpread = Random.insideUnitCircle * m_CurrentMaxSpread;
@@ -78,14 +104,24 @@ public abstract class WeaponBase : MonoBehaviour
         m_CurrentMaxSpread = Mathf.Clamp(m_CurrentMaxSpread + m_WeaponData.m_SpreadIncrement, m_WeaponData.m_BaseWeaponSpread, m_WeaponData.m_MaxWeaponSpread);
         return Value;
     }
+
+    /// <summary>
+    /// Takes ammo from the reserve and moves it into the clip.
+    /// </summary>
     protected void Reload() {
         int AmmoDelta = Mathf.Min(m_WeaponData.m_AmmoPerClip - m_AmmoInClip, m_ReserveAmmo - m_AmmoInClip);
 
         if (AmmoDelta > 0) {
             m_AmmoInClip += AmmoDelta;
+            m_ReserveAmmo -= AmmoDelta;
+            //TODO: Play m_ReloadSound Sound Clip.
         }
     }
 
+    /// <summary>
+    /// Decrements the Max Spread after an inital delay;
+    /// </summary>
+    /// <returns></returns>
     protected IEnumerator ResetSpread() {
         yield return new WaitForSeconds(SpreadResetDelay);
 
@@ -95,6 +131,10 @@ public abstract class WeaponBase : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Adds Ammo to the weapon's reserves
+    /// </summary>
+    /// <param name="amount"></param>
     public void GiveAmmo( int amount ) {
         m_ReserveAmmo = Mathf.Clamp(m_ReserveAmmo + amount, 0, m_WeaponData.m_MaxAmmo);
     }
